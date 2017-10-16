e3412575d4c221e1103896a51ef0f33424678da6
codexmrashid <mrashid@codexin.com>
2017-10-16 19:30:22 +0600

    sss

--- Corporate/js/main.js
+++ Corporate/js/main.js
@@ -3,40 +3,19 @@
 $(function() {
 
 
-// progressbar.js@1.0.0 version is used
-// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
-var bar = new ProgressBar.Circle('.skill_p', {
-  color: '#aaa',
-  // This has to be the same size as the maximum width to
-  // prevent clipping
-  strokeWidth: 4,
-  trailWidth: 1,
-  easing: 'easeInOut',
-  duration: 1400,
-  text: {
-    autoStyleContainer: false
-  },
-  from: { color: '#aaa', width: 1 },
-  to: { color: '#333', width: 4 },
-  // Set default step function for all animate calls
-  step: function(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
 
-    var value = Math.round(circle.value() * 80);
-    if (value === 0) {
-      circle.setText('');
-    } else {
-      circle.setText(value);
-    }
+  $('.circle').circleProgress({
+    value: 0.50,
+    size: 100,
+    fill: {
+      gradient: ["#fff", "#fff"]
+    },
+  });
 
-  }
-});
-bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-bar.text.style.fontSize = '2rem';
 
-bar.animate(0.80);  // Number from 0.0 to 1.0
+
+
 
 
 })

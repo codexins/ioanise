7fbeef69c1ed8ddc2233174be40a3eca3d3dcfbf
codexmrashid <mrashid@codexin.com>
2017-10-17 14:55:49 +0600

    blog

--- Corporate/js/main.js
+++ Corporate/js/main.js
@@ -5,14 +5,38 @@ $(function() {
 
 
 
-  $('.circle').circleProgress({
-    value: 0.50,
-    size: 100,
-    fill: {
-      gradient: ["#fff", "#fff"]
-    },
-  });
+    $('.circle').circleProgress({
+        value: 0.50,
+        size: 100,
+        fill: {
+            gradient: ["#fff", "#fff"]
+        },
+    });
 
+
+    // testimonial 
+
+    $('.testimonial-carosel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false ,
+    });
+    //  post gallery carosel 
+    $('.gallery-carosel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true ,
+        // nextArrow:'<button type="button" class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+        // prevArrow:'<button type="button" class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></button>'
+    
+        // prevArrow:'<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        // nextArrow:'<i class="fa fa-angle-right" aria-hidden="true"></i>'
+
+    });
 
 
 

33f659f7dd98c87ed9a7bda4bf672be64d2606f2
codexmrashid <mrashid@codexin.com>
2017-10-23 19:01:22 +0600

    puss

--- Corporate/assets/js/main.js
+++ Corporate/assets/js/main.js
@@ -6,8 +6,51 @@ $(function() {
 
 
 
+$('.counter').counterUp({
+    delay: 100,
+    time: 3000
+});
 
 
+
+
+
+    $('.portfolio-wrape').isotope({
+      // options
+      itemSelector: '.item',
+      // layoutMode: 'fitColumns',
+      columnWidth:  '.item',
+      
+    });
+
+  /**
+   * Slide left instantiation and action.
+   */
+      var slideLeft = new Menu({
+        wrapper: '#o-wrapper',
+        type: 'slide-left',
+        menuOpenerClass: '.c-button',
+        maskId: '#c-mask'
+      });
+
+      var slideLeftBtn = document.querySelector('#c-button--slide-left');
+      
+      slideLeftBtn.addEventListener('click', function(e) {
+        e.preventDefault;
+        slideLeft.open();
+      });
+
+
+
+    // reason-reason-to-choose
+
+    $('.reasons-to-choose').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: false ,
+    });
     // testimonial 
 
     $('.testimonial-carosel').slick({

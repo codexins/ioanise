db4e4ef4a8eb1bcdec98e8d6b58a9d9cf4bc3e1b
S.M. Rafiz <rafiz@codexin.com>
2017-11-30 17:56:51 +0600

    Some changes

--- Corporate/assets/js/main.js
+++ Corporate/assets/js/main.js
@@ -216,4 +216,20 @@ jQuery(document).ready(function($){
   });  //scrollup finished
 
 
-}); 
+ // For Parallax Effects
+(function(){
+
+    var parallax = document.querySelectorAll(".parallax"),
+        speed = 0.5;
+
+    window.onscroll = function(){
+        [].slice.call(parallax).forEach(function(el,i){
+            var windowYOffset = window.pageYOffset,
+            elementYOffset = el.offsetTop, elBackgrounPos = "50% " + ((windowYOffset - elementYOffset) * speed) + "px";
+            el.style.backgroundPosition = elBackgrounPos;
+        });
+    };
+})();
+
+
+});

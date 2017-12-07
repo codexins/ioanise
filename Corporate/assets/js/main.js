8a4196a18f35ee54b32ce07117ffaee52b3b161f
Zaman <aktarzaman84@gmail.com>
2017-12-07 12:06:05 +0600

    more change

--- Corporate/assets/js/main.js
+++ Corporate/assets/js/main.js
@@ -170,14 +170,23 @@ jQuery(document).ready(function($){
           autoplay :true ,
            responsive: [
               {
-                breakpoint: 700,
+                breakpoint: 992,
                 settings: {
                   slidesToShow: 4,
-                  slidesToScroll: 2,
+                  slidesToScroll: 1,
                 }
               },
+
               {
-                breakpoint: 480,
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                }
+              },
+
+              {
+                breakpoint: 481,
                 settings: {
                   slidesToShow: 2,
                   slidesToScroll: 1, 

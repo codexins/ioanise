2ae6f9864be98674f2a8e2151904a003df7b26ad
Zaman <aktarzaman84@gmail.com>
2018-01-08 18:49:59 +0600

    more

--- Corporate/assets/js/main.js
+++ Corporate/assets/js/main.js
@@ -1,75 +1,80 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
-    'use strict';
+    "use strict";
 
     /************************************************************
       image - SVG inline
     *************************************************************/
 
-    
-        $('.logo img').each(function(){
-            var $img = $(this);
-            var imgID = $img.attr('id');
-            var imgClass = $img.attr('class');
-            var imgURL = $img.attr('src');
-        
-            $.get(imgURL, function(data) {
-                // Get the SVG tag, ignore the rest
-                var $svg = $(data).find('svg');
-        
-                // Add replaced image's ID to the new SVG
-                if(typeof imgID !== 'undefined') {
-                    $svg = $svg.attr('id', imgID);
-                }
-                // Add replaced image's classes to the new SVG
-                if(typeof imgClass !== 'undefined') {
-                    $svg = $svg.attr('class', imgClass+' replaced-svg');
-                }
-        
-                // Remove any invalid XML tags as per http://validator.w3.org
-                $svg = $svg.removeAttr('xmlns:a');
-                
-                // Check if the viewport is set, else we gonna set it if we can.
-                if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-                }
-        
-                // Replace image with new SVG
-                $img.replaceWith($svg);
-        
-            }, 'xml');
-        
-        });
+
+    $('.logo img').each(function() {
+        var $img = $(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        $.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = $(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, else we gonna set it if we can.
+            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+    });
 
     /************************************************************
         Skin Switcher
     *************************************************************/
 
-        $('#colorPanel').ColorPanel({
-            styleSheet: '#cpswitch',
-            // animateContainer: '#o-wrapper',
-            colors: {
-                'assets/img/skins/default.png': 'assets/css/main.css',
-                'assets/img/skins/florence.png': 'assets/css/main-florence.css',
-                'assets/img/skins/rose.png': 'assets/css/main-rose.css',
-                'assets/img/skins/antonio.png': 'assets/css/main-antonio.css',
-                'assets/img/skins/radcliffe.png': 'assets/css/main-radcliffe.css',
-                'assets/img/skins/periwinkle.png': 'assets/css/main-periwinkle.css',
-            }
-        });
-    
+    $('#colorPanel').ColorPanel({
+        styleSheet: '#cpswitch',
+        // animateContainer: '#o-wrapper',
+        colors: {
+            'assets/img/skins/default.png': 'assets/css/main.css',
+            'assets/img/skins/florence.png': 'assets/css/main-florence.css',
+            'assets/img/skins/rose.png': 'assets/css/main-rose.css',
+            'assets/img/skins/antonio.png': 'assets/css/main-antonio.css',
+            'assets/img/skins/radcliffe.png': 'assets/css/main-radcliffe.css',
+            'assets/img/skins/periwinkle.png': 'assets/css/main-periwinkle.css',
+        }
+    });
+
 
     /************************************************************
         Superfish Menu
     *************************************************************/
     $(".sf-menu").superfish({
 
-        delay:       0,                            // one second delay on mouseout
-        animation: {opacity: 'show', height: 'show'},
-        animationOut: {opacity: 'hide'},
-        speed:       'fast',                          // faster animation speed
-        autoArrows:  false,
-        disableHI: true, 
+        delay: 0, // one second delay on mouseout
+        animation: {
+            opacity: 'show',
+            height: 'show'
+        },
+        animationOut: {
+            opacity: 'hide'
+        },
+        speed: 'fast', // faster animation speed
+        autoArrows: false,
+        disableHI: true,
 
     });
 
@@ -77,20 +82,23 @@ jQuery(document).ready(function($){
     $('.sub-menu').hover(function() {
         var menu = $(this);
         var child_menu = $(this).find('ul');
-        if( $(menu).offset().left + $(menu).width() + $(child_menu).width() > $(window).width() ){
-            $(child_menu).css({"left": "inherit", "right": "100%"});
-           }        
+        if ($(menu).offset().left + $(menu).width() + $(child_menu).width() > $(window).width()) {
+            $(child_menu).css({
+                "left": "inherit",
+                "right": "100%"
+            });
+        }
     });
 
-    
+
     /************************************************************
         Page Loader & intelligent Header Space
     *************************************************************/
 
-    $(window).on('load', function() { 
+    $(window).on('load', function() {
         $('.cx-pageloader').delay(300).fadeOut('fast');
 
-       // intelligent header
+        // intelligent header
         var intHeight = $('.intelligent-header').outerHeight();
         $('.intelligent-header-space').height(intHeight);
     });
@@ -103,27 +111,26 @@ jQuery(document).ready(function($){
     // grab an element
     var myElement = document.querySelector(".intelligent-header");
     // construct an instance of Headroom, passing the element
-    var headroom  = new Headroom(myElement);
+    var headroom = new Headroom(myElement);
     // initialise
     headroom.init();
 
-    
-    
-    
+
+
 
     $(window).scroll(function() {
         var height = $(window).scrollTop();
 
-        if(height  < 200) {
-            $('.intelligent-header').removeClass('scrolling-up'); 
+        if (height < 200) {
+            $('.intelligent-header').removeClass('scrolling-up');
         } else {
-            $('.intelligent-header').addClass('scrolling-up'); 
+            $('.intelligent-header').addClass('scrolling-up');
         }
     });
     /************************************************************
         Animated Counter
     *************************************************************/
-    if($('body').find('.counter').length !== 0) {
+    if ($('body').find('.counter').length !== 0) {
         $('.counter').counterUp({
             delay: 100,
             time: 3000
@@ -134,14 +141,14 @@ jQuery(document).ready(function($){
     /************************************************************
         Isotope Js for Portfolio Section
     *************************************************************/
-    if($('body').find('.portfolio-wrapper').length !== 0) {
+    if ($('body').find('.portfolio-wrapper').length !== 0) {
 
         var $isocontainer = $('.portfolio-wrapper');
 
         $isocontainer.imagesLoaded(function() {
             $isocontainer.isotope({
-                 itemSelector: ".portfolio",
-                 layoutMode: 'masonry',
+                itemSelector: ".portfolio",
+                layoutMode: 'masonry',
             });
 
         });
@@ -161,14 +168,14 @@ jQuery(document).ready(function($){
 
     }
 
-    
+
     //Targeting Portfolio a tag for click event
 
-    $(".portfolio .primary-title").click(function (e) {
+    $(".portfolio .primary-title").click(function(e) {
         $(this).find("a.clickable").first().click();
     });
 
-    $(".portfolio .primary-title a.clickable").click(function (e) {
+    $(".portfolio .primary-title a.clickable").click(function(e) {
         e.stopPropagation();
     });
 
@@ -193,29 +200,28 @@ jQuery(document).ready(function($){
     /************************************************************
         Reasons to Choose Carousel
     *************************************************************/
-    if($('body').find('.reasons-to-choose').length !== 0) {
+    if ($('body').find('.reasons-to-choose').length !== 0) {
         $('.reasons-to-choose').slick({
             infinite: true,
             slidesToShow: 3,
             slidesToScroll: 1,
             dots: true,
-            arrows: false ,
-            responsive: [
-                {
-                  breakpoint: 991,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true,
-                  }
+            arrows: false,
+            responsive: [{
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                    }
                 },
                 {
-                  breakpoint: 500,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                  }
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                    }
                 }
             ]
 
@@ -227,7 +233,7 @@ jQuery(document).ready(function($){
         Testimonial Carousel
     *************************************************************/
 
-    if($('body').find('.testimonial-carousel').length !== 0) { 
+    if ($('body').find('.testimonial-carousel').length !== 0) {
         $('.testimonial-carousel').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
@@ -242,22 +248,22 @@ jQuery(document).ready(function($){
             centerMode: true,
             focusOnSelect: true,
             responsive: [
-               
+
                 {
-                  breakpoint: 480,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1, 
-                  }
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
                 }
-              ]
+            ]
 
         });
     }
 
-    
+
     //Testimonial Carousel Type 02
-    if($('body').find('.testimonial-carousel-type-02').length !== 0) {
+    if ($('body').find('.testimonial-carousel-type-02').length !== 0) {
         $('.testimonial-carousel-type-02').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
@@ -267,9 +273,9 @@ jQuery(document).ready(function($){
     }
 
 
-     /************************************************************
-         Smooth Scroll to anchor tags
-     *************************************************************/
+    /************************************************************
+        Smooth Scroll to anchor tags
+    *************************************************************/
 
     $('.explore').bind('click', function() {
         $('html, body').stop().animate({
@@ -282,89 +288,87 @@ jQuery(document).ready(function($){
     /************************************************************
         Client Carousel
     *************************************************************/
-    if($('body').find('.client-section').length !== 0) {
-        if($('.client-section').hasClass('type-2')) {
+    if ($('body').find('.client-section').length !== 0) {
+        if ($('.client-section').hasClass('type-2')) {
 
-          $('.client-carousel').slick({
-              infinite: true,
-              slidesToShow: 5,
-              slidesToScroll: 1,
-              dots: false,
-              arrows: true ,
-              autoplay :true ,
-               responsive: [
-                  {
-                    breakpoint: 992,
-                    settings: {
-                      slidesToShow: 4,
-                      slidesToScroll: 1,
+            $('.client-carousel').slick({
+                infinite: true,
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: true,
+                autoplay: true,
+                responsive: [{
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 1,
+                        }
+                    },
+
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                        }
+                    },
+
+                    {
+                        breakpoint: 481,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1,
+                        }
                     }
-                  },
+                ]
 
-                  {
-                    breakpoint: 768,
-                    settings: {
-                      slidesToShow: 3,
-                      slidesToScroll: 1,
-                    }
-                  },
-
-                  {
-                    breakpoint: 481,
-                    settings: {
-                      slidesToShow: 2,
-                      slidesToScroll: 1, 
-                    }
-                  }
-              ]
-
-          });
+            });
         } else {
 
-        //client carosel type-1
-        $('.client-carousel').slick({
-            infinite: true,
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            dots: true,
-            arrows: false ,
-            autoplay :true,
-               responsive: [
-                  {
-                    breakpoint: 992,
-                    settings: {
-                      slidesToShow: 4,
-                      slidesToScroll: 1,
-                    }
-                  },
+            //client carosel type-1
+            $('.client-carousel').slick({
+                infinite: true,
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false,
+                autoplay: true,
+                responsive: [{
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 1,
+                        }
+                    },
 
-                  {
-                    breakpoint: 768,
-                    settings: {
-                      slidesToShow: 3,
-                      slidesToScroll: 1,
-                    }
-                  },
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                        }
+                    },
 
-                  {
-                    breakpoint: 481,
-                    settings: {
-                      slidesToShow: 2,
-                      slidesToScroll: 1, 
+                    {
+                        breakpoint: 481,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1,
+                        }
                     }
-                  }
-              ]
+                ]
 
-        });
+            });
         }
-    }    
-  
+    }
 
-  	/************************************************************
+
+    /************************************************************
         Scroll to Top JS 
     *************************************************************/
-    $(window).on('scroll',function () {
-        if($(window).scrollTop()>200) {
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
             $("#toTop").fadeIn();
         } else {
             $("#toTop").fadeOut();
@@ -372,50 +376,50 @@ jQuery(document).ready(function($){
     });
 
 
-	 $("#toTop").on('click', function() {
-	      $("html,body").animate({
-	          scrollTop:0
-	      }, 800)
-	  });  //scrollup finished
+    $("#toTop").on('click', function() {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 800)
+    }); //scrollup finished
 
 
-     /************************************************************
-         Placeholder JS
-     *************************************************************/
+    /************************************************************
+        Placeholder JS
+    *************************************************************/
 
 
-    
+
     $(function() {
         var input = document.createElement("input");
-        if(('placeholder' in input)==false) { 
+        if (('placeholder' in input) == false) {
             $('[placeholder]').focus(function() {
                 var i = $(this);
-                if(i.val() == i.attr('placeholder')) {
+                if (i.val() == i.attr('placeholder')) {
                     i.val('').removeClass('placeholder');
-                    if(i.hasClass('password')) {
+                    if (i.hasClass('password')) {
                         i.removeClass('password');
-                        this.type='password';
-                    }           
+                        this.type = 'password';
+                    }
                 }
             }).blur(function() {
-                var i = $(this);    
-                if(i.val() == '' || i.val() == i.attr('placeholder')) {
-                    if(this.type=='password') {
+                var i = $(this);
+                if (i.val() == '' || i.val() == i.attr('placeholder')) {
+                    if (this.type == 'password') {
                         i.addClass('password');
-                        this.type='text';
+                        this.type = 'text';
                     }
                     i.addClass('placeholder').val(i.attr('placeholder'));
                 }
             }).blur().parents('form').submit(function() {
                 $(this).find('[placeholder]').each(function() {
                     var i = $(this);
-                    if(i.val() == i.attr('placeholder'))
+                    if (i.val() == i.attr('placeholder'))
                         i.val('');
                 })
             });
         }
     });
-    
+
 
 
 
